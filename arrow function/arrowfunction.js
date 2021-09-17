077d3eaffea6d11abc71605cd1c8338465ea844a
vuduynhiennn <vuduynhiencute@gmail.com>
2021-09-18 02:21:30 +0700

    add file arrow function

---
+++ arrow function/arrowfunction.js
@@ -0,0 +1,19 @@
+function myFunction(login) {
+    console.log(login + ' ' + 'login sucessfully')
+}
+myFunction('user');
+
+
+const myFunction = (login) =>{
+    console.log(login + ' ' + 'login sucessfully')
+}
+myFunction('user');
+
+const sum = (a, b) => {
+    return a + b;
+}
+console.log(sum(2, 2));
+
+
+const sum = (a, b) => ({a: a, b: b});
+

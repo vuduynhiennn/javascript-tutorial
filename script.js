04af5a2e9fefb4aac2ff824af0471e5bd2e5189e
Vu Duy Nhien <vuduynhiencute@gmail.com>
2022-07-15 13:36:16 +0700

    setAtribute

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
+let listImg = document.getElementById('list--img')
 
-// SET ATRIBUTE
-// root.href = 'Học javascript tại fullstack.edu.vn'
-// root.atribute - chỉ thêm đc các atribute mà thẻ đó có thôi
-// root.setAttribute('href', 'Học lập trình tại F8');
+for (let i = 1; i <= 20; i++) {
+    let temp = document.createElement('canvas')
+    temp.setAttribute('id', `canvas${i}`)
+    listImg.appendChild(temp)
+}
 
-
-// GET ATRIBUTE
-// root.className; // get ra atribute có tên là class
-
-// element
-// htmlCollection
-// NodeList
-var root = document.getElementById('root');
-// innerText
-var para = root.querySelectorAll('.paragraph');
-
-var para1 = para[0];
-console.log(para1);
-// textContent

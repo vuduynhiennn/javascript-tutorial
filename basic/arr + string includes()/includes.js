e2a4f93e4efaad6db4594e3f3625bd4bacdfe88c
Vu Duy Nhien <vuduynhiencute@gmail.com>
2022-07-16 18:01:50 +0700

    clear

---
+++ basic/arr + string includes()/includes.js
@@ -0,0 +1,11 @@
+// for array
+let title = 'responsive web design'
+
+let index = 6
+// console.log(title.includes('web', index)) // 
+
+// for object
+let courses = ['javascript', 'php', 'java']
+
+// truyền số âm là  độ dài + số amm
+console.log(courses.includes('php', 2))

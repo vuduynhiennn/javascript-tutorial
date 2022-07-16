2fcb5be1d2e44e0c25211d161b4f32a70442e547
Vu Duy Nhien <vuduynhiencute@gmail.com>
2022-07-16 15:07:56 +0700

    sdf

---
+++ basic/readme.md
@@ -0,0 +1,2 @@
+https://fullstack.edu.vn/learning/javascript-co-ban?id=c7113b0b-3131-4dd1-ad5c-9a3ff81e0253
+code ra reduce2 giống với reduce, chưa kịp xem hãy nhớ xem lại
--- script.js
+++
@@ -1,8 +0,0 @@
-let listImg = document.getElementById('list--img')
-
-for (let i = 1; i <= 20; i++) {
-    let temp = document.createElement('canvas')
-    temp.setAttribute('id', `canvas${i}`)
-    listImg.appendChild(temp)
-}
-

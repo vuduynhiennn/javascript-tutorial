a6e2a6b65e33523f948b320ef8b0ba79776fd8c1
Vu Duy Nhien <vuduynhiencute@gmail.com>
2022-08-12 23:32:45 +0700

    cookie

---
+++ cookie/cookie.js
@@ -0,0 +1,28 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie)
+
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+setCookie('myName', 'Vu Duy Nhien', 1)
+getCookie('myName')
---
+++ cookie/test.js
@@ -0,0 +1,2 @@
+document.cookie = "username=s; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/cookie/index.html;"
+console.log(document.cookie)
